1718c358c2bac9364b64ab7e6b5894c1a06a4910
Ayush Kumar <ayush84050@gmail.com>
2025-03-27 16:31:10 +0000

    conversion

--- 01-basics/03_conversion.js
+++ 01-basics/03_conversion.js
@@ -1,13 +1,13 @@
 let score = "fdb33"
 
 
-console.log(typeof score);
+//console.log(typeof score);
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
+//console.log(typeof valueInNumber);
 
 
-console.log(valueInNumber);
+//console.log(valueInNumber);
 
 // "33" => 33
 //true => 1; false => 0
@@ -16,7 +16,7 @@ console.log(valueInNumber);
 let isLoggedIn = 0
 
 let booleanIsLoggesIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggesIn);
+//console.log(booleanIsLoggesIn);
 
 // 1 => true
 //"ayush" => true
@@ -24,7 +24,27 @@ console.log(booleanIsLoggesIn);
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+//console.log(typeof stringNumber);
+
+// ************  OPERATIONS  ***************
+
+let value = 3
+let negValue = -value
+
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Ayush"
+
+let str3 = str1 + str2
+console.log(str3);
 
 
 
