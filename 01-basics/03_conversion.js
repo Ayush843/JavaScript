2a377a121b2654cfee4c0dc62559d4262a85e0f1
Ayush Kumar <ayush84050@gmail.com>
2025-03-27 16:13:57 +0000

    conversion

---
+++ 01-basics/03_conversion.js
@@ -0,0 +1,30 @@
+let score = "fdb33"
+
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+
+console.log(valueInNumber);
+
+// "33" => 33
+//true => 1; false => 0
+// NaN -> Not a Number as fdb33 is not a number
+
+let isLoggedIn = 0
+
+let booleanIsLoggesIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggesIn);
+
+// 1 => true
+//"ayush" => true
+// " " => false
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+
+
+
