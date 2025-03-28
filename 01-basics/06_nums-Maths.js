0687dcb9b8f86d91328d369e780af51d81e45096
Ayush Kumar <ayush84050@gmail.com>
2025-03-28 18:39:15 +0000

    Maths

---
+++ 01-basics/06_nums-Maths.js
@@ -0,0 +1,36 @@
+const score = 400
+//console.log(score)
+
+const balance = new Number(100)
+//console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 23.8474
+
+//console.log(otherNumber.toPrecision(3));
+
+
+
+
+// ********  MATHS *********
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(2,4,34,56,2));
+// console.log(Math.max(2,4,34,56,2));
+
+console.log(Math.random());   //give number between 0-1
+console.log((Math.random()*10)+1); 
+console.log(Math.floor(Math.random()*10)+1); // can give number 0.1.... tp avoid this we have added +1(1-9)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
