fccf8781f4833760767dbef48d23f43264b45bd8
Ayush Kumar <ayush84050@gmail.com>
2025-03-29 12:49:47 +0000

    arrays

---
+++ 02-basics/01-arrays.js
@@ -0,0 +1,15 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4 ,5]
+const myHeros = ["ironman", "batman"]
+
+const myArr2 = new Array(1,2,3,4,5)
+console.log(myArr[2]);
+
+// Array methods
+
+myArr.push(6)
+console.log(myArr);
+
+
+
